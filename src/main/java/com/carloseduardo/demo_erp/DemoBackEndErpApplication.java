181ce9920455ddd1ab2d32c39071e6fa82ee97b5
Carlos Eduardo <kadumaroso@gmail.com>
2025-06-30 13:59:22 -0300

    Commit Inicial do Projeto

---
+++ src/main/java/com/carloseduardo/demo_erp/DemoBackEndErpApplication.java
@@ -0,0 +1,13 @@
+package com.carloseduardo.demo_erp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoBackEndErpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoBackEndErpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/carloseduardo/demo_erp/DemoBackEndErpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carloseduardo.demo_erp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoBackEndErpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
